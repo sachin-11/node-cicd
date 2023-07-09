7c437e6270fddfe476c4f5f3415b178ba2637237
sachin <rajeshsachin786@gmail.com>
2023-07-09 23:45:29 +0530

    first commit

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express();
 const port = 5000;
 
 // Define a basic route
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   res.send('Hello, this is my first deployment');
 });
 
 // Define a basic route
 app.get('/basic', (req, res) => {
-    res.send('Hello, this is my basic routes and deployment');
+    res.send('Hello, this is my basic routes and deployment..');
   });
 
 // Start the server
